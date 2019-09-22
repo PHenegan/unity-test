4d87bdcded2e5bbcc1283a39c16fd69ed2deab43
Patrick Henegan <phenegan03@gmail.com>
2019-09-21 21:25:29 -0400

    Basic Movement
    Added a cube that can move along the x and z axes

---
+++ unity-test/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    private static float xMovement;
+    private static float zMovement;
+    Vector3 movement;
+
+    /*
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+    */
+
+    // Update is called once per frame
+    void Update() {
+        xMovement = Input.GetAxis("Horizontal");
+        zMovement = Input.GetAxis("Vertical");
+
+        movement = new Vector3(xMovement, 0, zMovement);
+        transform.position += movement * Time.deltaTime;
+    }
+}
